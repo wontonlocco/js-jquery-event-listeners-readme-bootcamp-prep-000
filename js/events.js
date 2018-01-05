385353833408540cde48e8b9a0b83b160177f6c8
Ed Wright <ew_wrightiii@live.com>
2018-01-05 07:16:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
-
+function getIt(){
+  $('p').on("click", alert("Hey!"));
+}
+function frameIt(){
+    $('img').bind("load", function(){
+      $('img').addClass('tasty');
+    });
+  
+}
+function pressIt(){
+  $('input').keydown(function(event){
+    if(event.which == 71){
+      alert("You have pressed the letter G.");
+    }
+  })
+}
+function submitIt(){
+  $('form').submit(function(event){
+    alert('Your form is going to be submitted now.');
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+event.pressIt();
 });
